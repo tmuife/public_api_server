eec1dd32b61416dd8a7e7e7a6545e388e18b47e5
jinsh <43266626@qq.com>
2025-03-11 11:21:55 +0800

    add test

--- minicpm_server/app/tmp/feature_call.py
+++ minicpm_server/app/tmp/feature_call.py
@@ -1,5 +1,5 @@
 import time
-from datetime import datetime
+
 from decouple import config
 import os
 import requests
@@ -8,12 +8,10 @@ import asyncio
 from PIL import Image
 import base64
 import io
-import threading
 
-def call_image_query(batch, name, baseimg):
-    print(f"Run batch {batch},thread name {name}")
+async def call_query(content,question,api_url):
     api_key = config("API_KEY")
-    url = config("minicpm_url")+"image_query"
+    url = config("minicpm_url")+api_url
 
     # 设置请求头
     headers = {
@@ -22,7 +20,7 @@ def call_image_query(batch, name, baseimg):
         "access_token": api_key
     }
 
-    data = {"content": baseimg, "question": "Provide a description of the image in English."}
+    data = {"content": content, "question": question}
     # 发送 GET 请求
     response = requests.post(url, headers=headers, json=data)
     # 输出返回的结果
@@ -34,6 +32,7 @@ def call_image_query(batch, name, baseimg):
         print(jobj["data"]["text"])
     else:
         print(f"请求失败，状态码: {response.status_code}, 错误信息: {response.text}")
+
 def image_to_base64(image_path: str) -> str:
     # 打开图片
     with Image.open(image_path) as img:
@@ -47,25 +46,23 @@ def image_to_base64(image_path: str) -> str:
         base64_string = base64.b64encode(img_bytes).decode("utf-8")
         return base64_string
 
+def wav_to_base64(file_path: str) -> str:
+    with open(file_path, "rb") as f:
+        audio_bytes = f.read()
 
-if __name__ == "__main__":
+    # 将音频文件内容编码为 base64 字符串
+    base64_string = base64.b64encode(audio_bytes).decode("utf-8")
+    return base64_string
 
-    image_content = image_to_base64(image_path=config("test_image_path"))
-    # 获取当前时间
-    now = datetime.now()
-    print("当前时间是：", now)
-    for run_batch in range(0,config("run_times" ,cast=int)):
-        with open("concurrency.txt", "r", encoding="utf-8") as file:
-            concurrency = file.read()
-        print(concurrency)
-        thread_list: [threading.Thread] = []
-        for i in range(0,int(concurrency)):
-            thread_list.append(threading.Thread(target=call_image_query, args=(str(run_batch), "Thread-"+str(i), image_content)))
-        for thread in thread_list:
-            # 启动线程
-            thread.start()
-            # 等待所有线程完成
-            #thread.join()
-        print("所有线程执行完毕。")
-    now = datetime.now()
-    print("当前时间是：", now)
+#image_content = image_to_base64(image_path="/Users/walter/Downloads/猿人/4.jpg")
+#image_question = "Provide a description of the image in English."
+#asyncio.run(call_query(content=image_content,question=image_question,api_url="image_query"))
+
+
+audio_content = wav_to_base64(file_path='/Users/walter/Downloads/youtube/break.wav')
+audio_question = "What can you hear? In which second did the break occur?"
+asyncio.run(call_query(content=audio_content,question=audio_question,api_url="audio_query"))
+
+audio_content = wav_to_base64(file_path='/Users/walter/Downloads/youtube/scream.wav')
+audio_question = "What can you hear? In which second did the scream occur?"
+asyncio.run(call_query(content=audio_content,question=audio_question,api_url="audio_query"))
